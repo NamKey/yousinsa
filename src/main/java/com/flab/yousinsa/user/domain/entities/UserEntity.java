556ff43f70d4cbe0ed5c908812f5dd79a141c1ea
key <rlfbd5142@gmail.com>
2022-08-01 18:50:49 +0900

    [refactor] UserEntity Lombok 도입
    [refactor] User관련 Domain Component Lombok 도입

--- src/main/java/com/flab/yousinsa/user/domain/entities/UserEntity.java
+++ src/main/java/com/flab/yousinsa/user/domain/entities/UserEntity.java
@@ -1,8 +1,5 @@
 package com.flab.yousinsa.user.domain.entities;
 
-import java.time.LocalDateTime;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
@@ -16,89 +13,48 @@ import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.flab.yousinsa.global.common.BaseTimeEntity;
 import com.flab.yousinsa.store.domain.Store;
 import com.flab.yousinsa.user.domain.enums.UserRole;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @EntityListeners(AuditingEntityListener.class)
 @Table(name = "users")
-public class UserEntity {
+public class UserEntity extends BaseTimeEntity {
 
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
 	@NotNull
-	@Column(name = "user_name")
 	private String userName;
+
+	@Setter
 	@NotNull
 	@Email
-	@Column(name = "user_email")
 	private String userEmail;
+
 	@NotNull
 	@Column(name = "user_password")
 	private String userPassword;
+
+	@Setter
 	@Enumerated(value = EnumType.STRING)
-	@Column(name = "user_role")
 	private UserRole userRole;
-	@CreatedDate
-	@Column(name = "created_at")
-	private LocalDateTime createdAt;
-	@LastModifiedDate
-	@Column(name = "updated_at")
-	private LocalDateTime updatedAt;
 
-	@JsonIgnore
-	@OneToOne(mappedBy = "storeOwner", cascade = CascadeType.ALL)
+	@OneToOne(mappedBy = "storeOwner")
 	private Store store;
-
-	public UserEntity() {
-	}
-
-	public UserEntity(String userName, String userEmail, String userPassword, UserRole userRole) {
-		this.userName = userName;
-		this.userEmail = userEmail;
-		this.userPassword = userPassword;
-		this.userRole = userRole;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public String getUserName() {
-		return userName;
-	}
-
-	public String getUserEmail() {
-		return userEmail;
-	}
-
-	public void setUserEmail(String userEmail) {
-		this.userEmail = userEmail;
-	}
-
-	public String getUserPassword() {
-		return userPassword;
-	}
-
-	public UserRole getUserRole() {
-		return userRole;
-	}
-
-	public void setUserRole(UserRole userRole) {
-		this.userRole = userRole;
-	}
-
-	public LocalDateTime getCreatedAt() {
-		return createdAt;
-	}
-
-	public LocalDateTime getUpdatedAt() {
-		return updatedAt;
-	}
 }
