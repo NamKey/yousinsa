ef4e9b079a7dbe3fd74a34202125c20e0e0641f6
key <rlfbd5142@gmail.com>
2022-08-05 01:27:23 +0900

    [feature] Product DTO 필드 정의

--- src/main/java/com/flab/yousinsa/product/domain/dtos/ProductOptionDto.java
+++ src/main/java/com/flab/yousinsa/product/domain/dtos/ProductOptionDto.java
@@ -1,13 +1,19 @@
 package com.flab.yousinsa.product.domain.dtos;
 
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Builder
 @Getter
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ProductOptionDto {
+	private Long productOptionId;
 
+	private String productSize;
+
+	private Integer productCount;
 }
