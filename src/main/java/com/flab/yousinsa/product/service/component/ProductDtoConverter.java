3be421a2b4bed6ebae41e0efe5e6c22a4badf1a1
key <rlfbd5142@gmail.com>
2022-08-07 12:24:32 +0900

    [feature] Product Entity를 ProductDTO로 변환해주는 Converter

---
+++ src/main/java/com/flab/yousinsa/product/service/component/ProductDtoConverter.java
@@ -0,0 +1,32 @@
+package com.flab.yousinsa.product.service.component;
+
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Component;
+
+import com.flab.yousinsa.product.domain.dtos.ProductDto;
+import com.flab.yousinsa.product.domain.dtos.ProductOptionDto;
+import com.flab.yousinsa.product.domain.entity.ProductEntity;
+import com.flab.yousinsa.product.domain.entity.ProductOptionEntity;
+
+@Component
+public class ProductDtoConverter {
+	public ProductDto convertProductEntityToProductDto(ProductEntity productEntity) {
+		return ProductDto.builder()
+			.productId(productEntity.getId())
+			.productName(productEntity.getProductName())
+			.productOptions(
+				productEntity.getOptions().stream()
+					.map(this::convertProductOptionEntityToProductOptionDto)
+					.collect(Collectors.toList()))
+			.build();
+	}
+
+	public ProductOptionDto convertProductOptionEntityToProductOptionDto(ProductOptionEntity productOptionEntity) {
+		return ProductOptionDto.builder()
+			.productOptionId(productOptionEntity.getId())
+			.productSize(productOptionEntity.getProductSize())
+			.productCount(productOptionEntity.getProductCount())
+			.build();
+	}
+}
