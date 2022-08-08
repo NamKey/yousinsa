00b6a4950d2dd27148708a50312aa807a701bebf
key <rlfbd5142@gmail.com>
2022-08-08 20:52:14 +0900

    [feature] Product Domain Entity 정의
    [fix] Product와 ProductOptionEntity 양방향 매핑으로 변경

---
+++ src/main/java/com/flab/yousinsa/product/domain/entity/ProductEntity.java
@@ -0,0 +1,55 @@
+package com.flab.yousinsa.product.domain.entity;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+import com.flab.yousinsa.global.common.BaseTimeEntity;
+import com.flab.yousinsa.product.domain.enums.ProductCategory;
+import com.flab.yousinsa.store.domain.Store;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "products")
+public class ProductEntity extends BaseTimeEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Enumerated(value = EnumType.STRING)
+	private ProductCategory category;
+
+	@Min(value = 0)
+	private Long productPrice;
+
+	@NotBlank
+	private String productName;
+
+	@ManyToOne
+	@JoinColumn(name = "store_id")
+	private Store store;
+
+	@OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<ProductOptionEntity> options;
+}
---
+++ src/main/java/com/flab/yousinsa/product/domain/entity/ProductOptionEntity.java
@@ -0,0 +1,40 @@
+package com.flab.yousinsa.product.domain.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "product_options")
+public class ProductOptionEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(optional = false, fetch = FetchType.LAZY)
+	@JoinColumn(name = "product_id")
+	private ProductEntity product;
+
+	@Min(value = 0)
+	private Integer productCount;
+
+	@NotBlank
+	private String productSize;
+}
