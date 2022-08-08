9f052b3b49e490a0f3eda8fe2167c86fe5fe4c13
key <rlfbd5142@gmail.com>
2022-08-08 20:44:46 +0900

    [feature] PurchaseOrder Domain Controller Sketch

---
+++ src/main/java/com/flab/yousinsa/purchaseorder/controller/api/PurchaseController.java
@@ -0,0 +1,36 @@
+package com.flab.yousinsa.purchaseorder.controller.api;
+
+import java.net.URI;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.flab.yousinsa.purchaseorder.domain.dtos.CreatePurchaseOrderRequestDto;
+import com.flab.yousinsa.purchaseorder.service.contract.PurchaseOrderService;
+import com.flab.yousinsa.user.controller.annotation.AuthSession;
+import com.flab.yousinsa.user.controller.annotation.RolePermission;
+import com.flab.yousinsa.user.controller.annotation.SignInUser;
+import com.flab.yousinsa.user.domain.dtos.AuthUser;
+import com.flab.yousinsa.user.domain.enums.UserRole;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+public class PurchaseController {
+
+	private final PurchaseOrderService purchaseOrderService;
+
+	@AuthSession
+	@RolePermission(permittedRoles = {UserRole.BUYER})
+	@PostMapping("api/v1/orders")
+	public ResponseEntity<Void> createPurchaseOrder(
+		@RequestBody CreatePurchaseOrderRequestDto createPurchaseOrderRequestDto,
+		@SignInUser AuthUser user
+	) {
+		Long createdOrderId = 1L;
+		return ResponseEntity.created(URI.create("api/v1/orders/" + createdOrderId)).build();
+	}
+}
