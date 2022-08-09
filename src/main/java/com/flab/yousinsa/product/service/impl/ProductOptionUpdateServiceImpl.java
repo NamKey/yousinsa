859860328b2a90711c91f6ec1e4cc72350431897
key <rlfbd5142@gmail.com>
2022-08-09 13:03:31 +0900

    [feature] 요청된 수량만큼 빼는 업데이트를 수행하는 Service 작성

---
+++ src/main/java/com/flab/yousinsa/product/service/impl/ProductOptionUpdateServiceImpl.java
@@ -0,0 +1,55 @@
+package com.flab.yousinsa.product.service.impl;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
+
+import com.flab.yousinsa.global.exceptions.NotFoundException;
+import com.flab.yousinsa.product.domain.entity.ProductOptionEntity;
+import com.flab.yousinsa.product.repository.ProductOptionRepository;
+import com.flab.yousinsa.product.service.contract.ProductOptionUpdateService;
+import com.flab.yousinsa.product.service.exception.IllegalPurchaseRequestException;
+import com.flab.yousinsa.product.service.exception.OutOfStockException;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class ProductOptionUpdateServiceImpl implements ProductOptionUpdateService {
+
+	private final ProductOptionRepository productOptionRepository;
+
+	@Transactional
+	@Override
+	public Long sellProduct(Long productOptionId, int purchaseAmount) {
+		Assert.notNull(productOptionId, "product option id must be not null");
+		validatePurchaseRequest(purchaseAmount);
+
+		ProductOptionEntity requestedProductOption = productOptionRepository.findById(productOptionId)
+			.orElseThrow(() -> {
+				throw new NotFoundException("requested product option id does not exist");
+			});
+
+		validateProductStock(requestedProductOption.getProductCount(), purchaseAmount);
+
+		requestedProductOption.sell(purchaseAmount);
+
+		return requestedProductOption.getId();
+	}
+
+	private void validatePurchaseRequest(int purchaseAmount) {
+		if (purchaseAmount < 1) {
+			throw new IllegalPurchaseRequestException("purchase amount can not request under 0");
+		}
+	}
+
+	private void validateProductStock(int productStockCount, int purchaseAmount) {
+		if (productStockCount < 1) {
+			throw new OutOfStockException("requested product is out of stock");
+		}
+
+		if (productStockCount < purchaseAmount) {
+			throw new OutOfStockException("requested purchase amount is over product stock count");
+		}
+	}
+}
