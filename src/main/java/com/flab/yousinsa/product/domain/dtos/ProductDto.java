0be3578afa5c5096b0da69cdac2d2997091245e6
key <rlfbd5142@gmail.com>
2022-08-10 21:41:49 +0900

    [feature] ProductEntity를 나타내는 DTO Sketch

---
+++ src/main/java/com/flab/yousinsa/product/domain/dtos/ProductDto.java
@@ -0,0 +1,13 @@
+package com.flab.yousinsa.product.domain.dtos;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ProductDto {
+
+}
