cc9d4940454f2c7a1f3c2169e99953082a1a3d63
key <rlfbd5142@gmail.com>
2022-08-13 19:23:28 +0900

    [feature] User를 Fetch Join으로 갖고 오는 Query 작성
    [fix] Optional로 반환되는 Entity에 대한 Assert 문 수정

--- src/main/java/com/flab/yousinsa/store/domain/StoreRepository.java
+++ src/main/java/com/flab/yousinsa/store/domain/StoreRepository.java
@@ -1,6 +1,9 @@
 package com.flab.yousinsa.store.domain;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.flab.yousinsa.user.domain.entities.UserEntity;
@@ -9,4 +12,7 @@ import com.flab.yousinsa.user.domain.entities.UserEntity;
 public interface StoreRepository extends JpaRepository<Store, Long> {
 
 	boolean existsByStoreOwner(UserEntity user);
+
+	@Query(value = "SELECT s FROM Store s JOIN FETCH s.storeOwner")
+	Optional<Store> findByIdWithOwner(Long storeId);
 }
--- src/test/java/com/flab/yousinsa/store/domain/StoreRepositoryTest.java
+++ src/test/java/com/flab/yousinsa/store/domain/StoreRepositoryTest.java
@@ -1,5 +1,9 @@
 package com.flab.yousinsa.store.domain;
 
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import java.util.Optional;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -10,12 +14,16 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import com.flab.yousinsa.store.enums.StoreStatus;
 import com.flab.yousinsa.user.domain.entities.UserEntity;
 import com.flab.yousinsa.user.domain.enums.UserRole;
+import com.flab.yousinsa.user.repository.contract.UserRepository;
 
 @DataJpaTest
 class StoreRepositoryTest {
 
 	@Autowired
 	StoreRepository storeRepository;
+
+	@Autowired
+	UserRepository userRepository;
 
 	UserEntity user;
 
@@ -45,6 +53,29 @@ class StoreRepositoryTest {
 		// then
 		Assertions.assertEquals(createStore.getStoreName(), store.getStoreName());
 		Assertions.assertEquals(createStore.getStoreOwner().getUserName(), store.getStoreOwner().getUserName());
+	}
+
+	@Test
+	@DisplayName("Owner와 함께 상점 조회(Fetch Join Test)")
+	public void getStoreWithOwner() {
+		// given
+		userRepository.save(user);
+		Store createStore = Store.builder()
+			.storeName("store")
+			.storeOwner(user)
+			.storeStatus(StoreStatus.REQUESTED)
+			.build();
+		Store savedStore = storeRepository.save(createStore);
+
+		// when
+		Optional<Store> requestedStore = storeRepository.findByIdWithOwner(1L);
+
+		// then
+		assertThat(requestedStore)
+			.hasValue(savedStore)
+			.map(Store::getStoreOwner)
+			.hasValue(user);
+
 	}
 
 }
