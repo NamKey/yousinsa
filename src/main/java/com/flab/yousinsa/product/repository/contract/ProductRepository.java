3c25453e49dca9395cea9c33e5879c1371358d75
key <rlfbd5142@gmail.com>
2022-08-16 16:32:04 +0900

    [fix] BatchSize 문법 오적용, 전역 batch size 지정

--- src/main/java/com/flab/yousinsa/product/repository/contract/ProductRepository.java
+++ src/main/java/com/flab/yousinsa/product/repository/contract/ProductRepository.java
@@ -1,6 +1,5 @@
 package com.flab.yousinsa.product.repository.contract;
 
-import org.hibernate.annotations.BatchSize;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,7 +8,5 @@ import com.flab.yousinsa.product.domain.entity.ProductEntity;
 import com.flab.yousinsa.product.domain.enums.ProductCategory;
 
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
-
-	@BatchSize(size = 100)
 	Page<ProductEntity> findAllByCategory(ProductCategory category, Pageable pageable);
 }
