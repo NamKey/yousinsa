942c4a62c75115a6ea8664c28b06503b21ec3938
key <rlfbd5142@gmail.com>
2022-08-16 14:25:22 +0900

    [fix] Paging을 사용하는 부분이므로 BatchSize 조정을 통하여 OOM 방지

--- src/main/java/com/flab/yousinsa/product/repository/contract/ProductRepository.java
+++ src/main/java/com/flab/yousinsa/product/repository/contract/ProductRepository.java
@@ -1,5 +1,6 @@
 package com.flab.yousinsa.product.repository.contract;
 
+import org.hibernate.annotations.BatchSize;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,5 +9,7 @@ import com.flab.yousinsa.product.domain.entity.ProductEntity;
 import com.flab.yousinsa.product.domain.enums.ProductCategory;
 
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+
+	@BatchSize(size = 100)
 	Page<ProductEntity> findAllByCategory(ProductCategory category, Pageable pageable);
 }
