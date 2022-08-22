3784f01fbb77019cd2ee3bb089a0d963c80a3804
key <rlfbd5142@gmail.com>
2022-08-23 03:11:43 +0900

    [fix] Spring Boot 기본 DataSource설정 제외

--- src/main/java/com/flab/yousinsa/YousinsaApplication.java
+++ src/main/java/com/flab/yousinsa/YousinsaApplication.java
@@ -2,12 +2,11 @@ package com.flab.yousinsa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class YousinsaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(YousinsaApplication.class, args);
 	}
-
 }
