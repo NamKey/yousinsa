c820e74882ba319584a68a4a12e4cf6cf6d5293f
key <rlfbd5142@gmail.com>
2022-08-30 11:27:56 +0900

    [fix] Redisson Client에서 Lettuce Client로 변경

--- src/main/java/com/flab/yousinsa/global/config/RedisCacheConfig.java
+++ src/main/java/com/flab/yousinsa/global/config/RedisCacheConfig.java
@@ -1,19 +1,19 @@
 package com.flab.yousinsa.global.config;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.redisson.Redisson;
-import org.redisson.api.RedissonClient;
-import org.redisson.config.Config;
-import org.redisson.spring.cache.CacheConfig;
-import org.redisson.spring.cache.RedissonSpringCacheManager;
-import org.redisson.spring.data.connection.RedissonConnectionFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @EnableCaching
 @Configuration
@@ -23,27 +23,40 @@ public class RedisCacheConfig {
 	String REDIS_HOST;
 
 	@Value("${spring.redis.port}")
-	String REDIS_PORT;
+	Integer REDIS_PORT;
 
-	@Bean(destroyMethod = "shutdown")
-	public RedissonClient redissonClient() {
-		Config redisConfig = new Config();
-		redisConfig.useSingleServer()
-			.setAddress("redis://" + REDIS_HOST + ":" + REDIS_PORT);
-		return Redisson.create(redisConfig);
+	@Bean
+	public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+		RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+
+		redisTemplate.setEnableTransactionSupport(true);
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+		redisTemplate.setValueSerializer(new StringRedisSerializer());
+		redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+		redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+		redisTemplate.setConnectionFactory(connectionFactory);
+		return redisTemplate;
 	}
 
 	@Bean
-	public RedissonConnectionFactory redisConnectionFactory(RedissonClient redissonClient) {
-		return new RedissonConnectionFactory(redissonClient);
+	public RedisConnectionFactory redisConnectionFactory() {
+		RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+		redisStandaloneConfiguration.setHostName(REDIS_HOST);
+		redisStandaloneConfiguration.setPort(REDIS_PORT);
+		return new LettuceConnectionFactory(redisStandaloneConfiguration);
 	}
 
 	@Bean
-	public CacheManager cacheManager(RedissonClient redissonClient) {
-		Map<String, CacheConfig> config = new HashMap<>();
+	public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+		RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+			.serializeKeysWith(
+				RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+			.serializeValuesWith(
+				RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
 
-		// create "caching" spring cache with ttl = 1 minutes and maxIdleTime = 12 minutes
-		config.put("caching", new CacheConfig(1 * 60 * 1000, 12 * 60 * 1000));
-		return new RedissonSpringCacheManager(redissonClient, config);
+		return RedisCacheManager.RedisCacheManagerBuilder
+			.fromConnectionFactory(redisConnectionFactory)
+			.cacheDefaults(redisCacheConfiguration)
+			.build();
 	}
 }
