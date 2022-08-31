b915119a7719a39c41a29c8a81b438d961442f3b
key <rlfbd5142@gmail.com>
2022-08-31 23:00:45 +0900

    [fix] Redis에서 Transaction 사용하지 않도록 변경

--- src/main/java/com/flab/yousinsa/global/config/RedisCacheConfig.java
+++ src/main/java/com/flab/yousinsa/global/config/RedisCacheConfig.java
@@ -16,6 +16,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @EnableCaching
@@ -29,12 +30,11 @@ public class RedisCacheConfig {
 	Integer REDIS_PORT;
 
 	@Bean
-	public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
-		RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
 
-		redisTemplate.setEnableTransactionSupport(true);
 		redisTemplate.setKeySerializer(new StringRedisSerializer());
-		redisTemplate.setValueSerializer(new StringRedisSerializer());
+		redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
 		redisTemplate.setHashKeySerializer(new StringRedisSerializer());
 		redisTemplate.setHashValueSerializer(new StringRedisSerializer());
 		redisTemplate.setConnectionFactory(connectionFactory);
